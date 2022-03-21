9a93c14d94c3426cb21c4a133aa6cf701f0fb865
Kiran <kiransettyks@gmail.com>
2022-03-21 23:01:15 +0200

    Implementation of 2nd Player Create a label to display who's turn it is and if someone won Fixed bugs in logic of grid

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ window.title("Tic Tac Toe")
 window.config(padx=50, pady=50, bg=WHITE)
 
 blank_img = PhotoImage(file='blank.png')
-circle_img = PhotoImage(file='circle.png')
-cross_img = PhotoImage(file='cross.png')
+player_img = [PhotoImage(file='cross.png'), PhotoImage(file='circle.png')]
+
+label = Label(text="Player 1's Turn", font=("Courier", 30, 'bold'), bg='white', fg='black', height=1)
+label.grid(row=0, columnspan=3)
 
 canvas = Canvas(width=600, height=600, bg=WHITE)
 grid_img = PhotoImage(file='grid.png')
 canvas.create_image(250, 250, image=grid_img)
-canvas.place(x=0, y=0)
+canvas.place(x=0, y=52)
 
 active = [None, None]
 
+player = False
 
 class Grid:
     def __init__(self):
@@ -31,15 +34,22 @@ class Grid:
     def check_vrt(self, i):
         return self.values[0][i] == self.values[1][i] and self.values[1][i] == self.values[2][i]
 
-    def check_cross(self, i):
-        if i % 2 == 1:
-            return False
-        elif i == 2:
-            return self.values[0][2] == self.values[1][1] and self.values[1][1] == self.values[2][0] and \
-                   self.values[0][2] != 0
-        else:
+    def check_cross(self, sum, i):
+        def check_1():
+            return self.values[0][2] == self.values[1][1] and self.values[1][1] == self.values[2][0] and self.values[0][2] != 0
+
+        def check_2():
             return self.values[0][0] == self.values[1][1] and self.values[1][1] == self.values[2][2] and \
                    self.values[2][2] != 0
+
+        if sum % 2 == 1:
+            return False
+        elif i == 1:
+            return check_1() or check_2()
+        elif sum == 2:
+            return check_1()
+        else:
+            return check_2()
 
     def check_value(self, i, j):
         if self.values[i][j] == 0:
@@ -49,6 +59,11 @@ class Grid:
 
     def change_value(self, i, j, value):
         self.values[i][j] = value
+
+    def disable(self):
+        for i in range(3):
+            for j in range(3):
+                self.values[i][j] = 3
 
     def print(self):
         print(self.values[0])
@@ -64,19 +79,27 @@ class Btn:
         self.row = j
         self.column = i
         self.grid_btn = Button(command=self.press, image=blank_img)
-        self.grid_btn.grid(padx=BUTTON_PADX, pady=BUTTON_PADY, column=i, row=j)
+        self.grid_btn.grid(padx=BUTTON_PADX, pady=BUTTON_PADY, column=i, row=j+1)
 
     def press(self):
         if logic.check_value(self.row, self.column):
-            self.grid_btn.configure(image=cross_img)
-            logic.change_value(self.row, self.column, 5)
+            global player
+            self.grid_btn.configure(image=player_img[int(player)])
+            print(int(player))
+            logic.change_value(self.row, self.column, int(player)+1)
             logic.print()
             print(f"{self.row} {self.column}")
-            if logic.check_hzn(self.row) or logic.check_vrt(self.column) or logic.check_cross(self.row+self.column):
-                print('You Win!')
+            if logic.check_hzn(self.row) or logic.check_vrt(self.column) or logic.check_cross(self.row+self.column, self.row):
+                print(f'Player {int(player)+1} Wins!')
+                label.configure(text=f'Player {int(player)+1} Wins!')
                 print(logic.check_hzn(self.row))
                 print(logic.check_vrt(self.column))
-                print(logic.check_cross(self.row+self.column))
+                print(logic.check_cross(self.row+self.column, self.row))
+                logic.disable()
+                game = False
+            else:
+                player = not player
+                label.configure(text=f"Player {int(player)+1}'s Turn")
         else:
             print("Error")
 
